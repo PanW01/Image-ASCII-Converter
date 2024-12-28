da86017c7ffcf18f4d7f2730610e0f6a06928512
Pandev <pansoldier.w@gmail.com>
2024-12-28 20:16:04 +0100

    🌟Add: Create initial shape generator functionality

--- image_to_ascii.py
+++ image_to_ascii.py
@@ -1,4 +1,6 @@
 from PIL import Image # type: ignore
+from typing import Optional
+import shapes
 import math
 
 class ASCIIGenerator:
@@ -6,10 +8,11 @@ class ASCIIGenerator:
     def __init__(self, imagePath):
         self.image = Image.open(imagePath)
 
-    def draw_ascii(self, draw_type):
+    def draw_ascii(self, draw_type: str, shape: Optional[str] = None):
         image = self.image
         image = self.resize_with_aspect_ratio(image, 128)
         image = self.normalize_image(image)
+        image = self.include_shape(image, shape)
 
         drawing_method = self.select_drawing_type(draw_type)
         picture = drawing_method.convert_to_ascii(image)
@@ -28,6 +31,26 @@ class ASCIIGenerator:
         normalize_width = width * 2
 
         return image.resize((normalize_width, height))
+
+    def include_shape(self, image, shape):
+        shape = shapes.Stair(image)
+        star_position = shape.draw_shape()
+        pixel_list = list(image.getdata())
+        new_image = Image.new("L", image.size, 255)
+        
+        for y in range(image.height): # It spends a lot of time because is a loop with the original image size
+            for x in range(image.width):
+                if (x, y) in star_position:
+                    index = y * image.width + x
+                    pixel_value = pixel_list[index]
+                    r, g, b = pixel_value
+                    gray_value = int(0.299 * r + 0.587 * g + 0.114 * b)
+                    new_image.putpixel((x, y), gray_value)
+
+
+        return new_image
+
+
 
     def select_drawing_type(self, draw_type):
         draw_type = draw_type.lower()
@@ -53,7 +76,7 @@ class ASCIIGenerator:
     class GrayScaleDrawing:
         def convert_to_ascii(self, image):
             image = image.convert("L")
-            characters = ["█", "▓", "▒", "░"]
+            characters = ["█", "▓", "▒", "░", "   "]
             pixelList = list(image.getdata())
             asciiPicture = list()
             for i in range(0, len(pixelList), image.width):
